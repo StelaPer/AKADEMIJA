1e3d2ed9ea4c6c10244c8b823f507aa3cf3f2757
Stela Pervenyte <Stela.Pervenyte@stud.vtmc.lt>
2022-09-30 18:30:23 +0300

    Master Branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld { 
 public static void main (String [] args) { 
 System.out.println("Hello, World!");
-System.out.println("Labs, Pasauli!");
+System.out.println("LLabas, Pasauli!");
 }
 }
